6545cbf234df00e351565e5519140990d78c3ab5
Ansh Tripathi <yashansh295@gmail.com>
2026-06-25 01:23:04 +0530

    MongoDB connection completed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const experess = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const connectDB =require('./config/db');
 const dotenv= require('dotenv');
 dotenv.config();
+connectDB();
 const app = experess();
 app.use(cors());
 app.use(experess.json());
@@ -17,7 +19,7 @@ app.get('/api/test',(req,res)=>{
         message:"Backend Working"
     });
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
